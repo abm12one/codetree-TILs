57bc481bc349e3be22924fe376d2344d418137ca
abm12one <87717167+abm12one@users.noreply.github.com>
2024-01-21 17:42:49 +0900

    Title: 정수 찾기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240121/정수 찾기/find-an-integer.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<limits.h>
+#include<cassert>
+#include<unordered_map>
+#include<map>
+#include <unordered_set>
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	unordered_set<int>s;
+	int n,m;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		int t;
+		cin>>t;
+		
+		s.insert(t);
+	}
+	cin>>m;
+	for(int i=0;i<m;i++){
+		int temp;
+		cin>>temp;
+		if(s.find(temp)==s.end()){
+			cout<<'0'<<' ';
+		}
+		else{
+			cout<<'1'<<' ';
+		}
+		
+	}
+	
+
+}
