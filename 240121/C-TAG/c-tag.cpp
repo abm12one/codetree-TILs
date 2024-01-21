4e6ac1c11268539f2456e84e76f883675981d979
abm12one <87717167+abm12one@users.noreply.github.com>
2024-01-21 22:42:07 +0900

    Title: C-TAG, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240121/C-TAG/c-tag.cpp
+++ 240121/C-TAG/c-tag.cpp
@@ -13,6 +13,33 @@
 #include <unordered_set>
 
 using namespace std;
+int n,m;
+vector<string>a;
+vector<string>b;
+int sum=0;
+
+void isok(int x,int y,int z){
+	
+	unordered_set<string>s;
+	
+	for(int i=0;i<n;i++){
+		string temp="";
+		temp+=a[i][x]+a[i][y]+a[i][z];
+		s.insert(temp);
+	}
+	
+	for(int i=0;i<n;i++){
+		string temp="";
+		temp+=b[i][x]+b[i][y]+b[i][z];
+		if(s.find(temp)!=s.end()){
+			return;
+		}
+	}
+	
+	sum++;
+	return;
+	
+}
 
 int main(){
 
@@ -20,42 +47,22 @@ int main(){
 	cin.tie(NULL);
 	cout.tie(NULL);
 	
-	int n,m;
+	
 	cin>>n>>m;
-	vector<string>a(n);
-	vector<string>b(n);
+	a=vector<string>(n);
+	b=vector<string>(n);
 	for(int i=0;i<n;i++){
 		cin>>a[i];
 	}
 	for(int i=0;i<n;i++){
 		cin>>b[i];
 	}
-	int sum=0;
+	sum=0;
 	for(int i=0;i<m-2;i++){
 		for(int j=i+1;j<m-1;j++){
 			for(int k=j+1;k<m;k++){
 				
-				unordered_set<string>all;
-				unordered_set<string>arr1;
-				unordered_set<string>arr2;
-				
-				for(int r=0;r<n;r++){
-					string temp1="";
-					temp1+=a[r][i];
-					temp1+=a[r][j];
-					temp1+=a[r][k];
-					string temp2="";
-					temp2+=b[r][i];
-					temp2+=b[r][j];
-					temp2+=b[r][k];
-					all.insert(temp1);
-					all.insert(temp2);
-					arr1.insert(temp1);
-					arr2.insert(temp2);
-				}
-				if(all.size()==arr1.size()+arr2.size()){
-					sum++;
-				}
+				isok(i,j,k);
 				
 				
 			}
