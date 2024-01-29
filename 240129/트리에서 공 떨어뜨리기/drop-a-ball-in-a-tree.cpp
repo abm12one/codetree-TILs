dd38e1c498b0d1142b0a09e3939d30d1d5dc5ec4
abm12one <87717167+abm12one@users.noreply.github.com>
2024-01-29 22:45:31 +0900

    Title: 트리에서 공 떨어뜨리기, Time: 271ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240129/트리에서 공 떨어뜨리기/drop-a-ball-in-a-tree.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<limits.h>
+#include<cassert>
+#include<unordered_map>
+#include<map>
+#include <unordered_set>
+#include<set>
+
+using namespace std;
+
+vector<int>le;
+vector<int>ri;
+int n;
+int put(int r,int k){
+	
+	if(le[r]==-1&&ri[r]==-1){
+		return r;
+	}
+	else if(le[r]==-1){
+		put(ri[r],k);
+	}
+	else if(ri[r]==-1){
+		put(ri[r],k);
+	}
+	else{
+		if(k%2==0){
+			put(le[r],k/2);
+		}
+		else{
+			put(le[r],k/2+1);	
+		}
+	}
+	
+	
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	
+	cin>>n;
+	
+	le=vector<int>(n+1);
+	ri=vector<int>(n+1);
+	for(int i=1;i<n+1;i++){
+		cin>>le[i]>>ri[i];
+	}
+	int k;
+	cin>>k;
+	cout<<put(1,k)<<"\n";
+	
+}
