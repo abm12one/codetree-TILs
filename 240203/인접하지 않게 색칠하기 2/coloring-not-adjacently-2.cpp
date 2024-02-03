a8a29a58e0507b411a38578336e950edb09645ac
abm12one <87717167+abm12one@users.noreply.github.com>
2024-02-03 15:58:57 +0900

    Title: 인접하지 않게 색칠하기 2, Time: 187ms, Memory: 66MB, Status: Passed - CodeTree

--- 240203/인접하지 않게 색칠하기 2/coloring-not-adjacently-2.cpp
+++ 240203/인접하지 않게 색칠하기 2/coloring-not-adjacently-2.cpp
@@ -17,39 +17,48 @@ using namespace std;
 vector<vector<int>>m;
 vector<int>arr;
 vector<int>visit;
+vector<vector<vector<int>>>dp;
 int n,k;
-int ans;
-void dfs(int now,int num,int sum){
-	
-	
-	if(num==4){
-		ans=max(ans,sum);
-		return ;
-	}
-	
-	
-	
-	for(int i=0;i<m[now].size();i++){
-		int there=m[now][i];
-		visit[there]++;
-	}
-	
-	for(int i=now+1;i<n+1;i++){
-		if(visit[i]==0){
-			visit[i]++;
-			dfs(i,num+1,sum+arr[now]);
-			visit[i]--;
-		}
 
+void dfs(int n){
+	visit[n]=1;
+	int left=0;
+	int right=0;
+	for(int i=0;i<m[n].size();i++){
+		int there=m[n][i];
+		if(visit[there]==1)continue;
+		
+		
+		dfs(there);
+		
+		if(left==0){
+			left=there;
+		}
+		else{
+			right=there;
+		}
+		
 	}
 	
-	for(int i=0;i<m[now].size();i++){
-		int there=m[now][i];
-		visit[there]--;
+	dp[n][1][1]=arr[n];
+	dp[n][0][0]=0;
+	
+	if(left!=0 && right!=0){
+		for(int i=1;i<=k;i++){
+			for(int j=0;j<i;j++){
+				dp[n][i][1]=max(dp[n][i][1],dp[left][j][0]+dp[right][i-j-1][0]+arr[n]);
+			}	
+		}
+		for(int i=0;i<=k;i++){
+			for(int j=0;j<=i;j++){
+				dp[n][i][0]=max(dp[n][i][0],max(dp[left][j][0],dp[left][j][1])+max(dp[right][i-j][0],dp[right][i-j][1]));
+			}
+		}	
 	}
-	return;
+	
 	
 }
+
 
 int main(){
 
@@ -58,9 +67,13 @@ int main(){
 	cout.tie(NULL);
 	
 	cin>>n;
+	
 	m=vector<vector<int>>(n+1);
 	arr=vector<int>(n+1);
 	visit=vector<int>(n+1);
+	
+	
+	
 	for(int i=0;i<n-1;i++){
 		int a,b;
 		cin>>a>>b;
@@ -75,11 +88,15 @@ int main(){
 	
 	
 	cin>>k;
+	dp=vector<vector<vector<int>>>(n+1,vector<vector<int>>(k+1,vector<int>(2,0)));
 	
-	for(int i=1;i<=n;i++){
-		visit[i]++;
-		dfs(i,0,arr[i]);
-		visit[i]++;
+	dfs(1);
+	
+	int ans=0;
+	for(int i=1;i<=k;i++){
+		ans=max(ans,dp[1][k][0]);
+		ans=max(ans,dp[1][k][1]);
 	}
-	cout<<ans;
+	cout<<ans<<'\n';
+	
 }
