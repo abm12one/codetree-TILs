b589c78583064b82bcc47347ce7a1523c02d3182
abm12one <87717167+abm12one@users.noreply.github.com>
2024-02-06 22:48:47 +0900

    Title: 트리 완성, Time: 55ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240206/트리 완성/tree-completion.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<limits.h>
+#include<cassert>
+#include<unordered_map>
+#include<map>
+#include <unordered_set>
+#include<set>
+
+using namespace std;
+
+vector<int>p;
+int find(int a){
+	if(p[a]==a){
+		return p[a];
+	}
+	
+	return p[a]=find(p[a]);
+}
+
+int uset(int a,int b){
+	
+	int fa=find(a);
+	int fb=find(b);
+	if(fa==fb){
+		return 1;
+	}
+	
+	if(fb<fa){
+		p[fa]=fb;
+		
+	}
+	else{
+		p[fb]=fa;
+	}
+	
+	return 0;
+	
+	
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n,m;
+	cin>>n>>m;
+	
+	p=vector<int>(n+1);
+	for(int i=0;i<n+1;i++){
+		p[i]=i;
+	}
+	int sum=0;
+	
+	for(int i=0;i<m;i++){
+		int a,b;
+		cin>>a>>b;
+		if(uset(a,b)==1){
+			sum++;
+		}
+	}
+	
+	for(int i=1;i<n+1;i++){
+		if(find(i)!=find(1)){
+			sum++;
+			uset(i,1);
+		}
+	}
+	
+	cout<<sum;
+	
+}
