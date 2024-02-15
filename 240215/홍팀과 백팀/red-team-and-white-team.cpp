2af05079d7b34a31ec6182eed7af5a990855dbe1
abm12one <87717167+abm12one@users.noreply.github.com>
2024-02-15 22:24:33 +0900

    Title: 홍팀과 백팀, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240215/홍팀과 백팀/red-team-and-white-team.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<limits.h>
+#include<cassert>
+#include<unordered_map>
+#include<map>
+#include <unordered_set>
+#include<set>
+
+using namespace std;
+
+vector<int>p;
+vector<int>enemy;
+
+int find(int a){
+	if(p[a]==a){
+		return p[a];
+	}
+	
+	return p[a]=find(p[a]);
+}
+
+void uset(int a,int b){
+	
+	int fa=find(a);
+	int fb=find(b);
+	if(fa==fb){
+		return ;
+	}	
+	p[fb]=fa;
+	return ;
+	
+}
+
+
+
+
+
+
+
+int main(){
+
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	
+	int n,m;
+	cin>>n>>m;
+	p=vector<int>(n+1);
+	enemy=vector<int>(n+1,-1);
+	
+	for(int i=0;i<n+1;i++){
+		p[i]=i;
+	}
+	
+	int flag;
+	for(int i=0;i<m;i++){
+		int a,b;
+		cin>>a>>b;
+		
+		if(find(a)==find(b)){
+			flag=1;
+			break;
+		}
+		if(enemy[b]!=-1) uset(a,enemy[b]);
+		if(enemy[a]!=-1) uset(b,enemy[a]);
+		
+		enemy[a]=find(b);
+		enemy[b]=find(a);	
+			
+	}
+	
+	if(flag==1){
+		cout<<0<<'\n';
+	}
+	else{
+		cout<<1<<'\n';
+	}
+	
+	
+	
+}
