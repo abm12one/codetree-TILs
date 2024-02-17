051ab8f6624edae8672da5c65ee41aedf2d474e4
abm12one <87717167+abm12one@users.noreply.github.com>
2024-02-17 22:28:55 +0900

    Title: 노드 맞추기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240217/노드 맞추기/nodes-guessing.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<limits.h>
+#include<cassert>
+#include<unordered_map>
+#include<map>
+#include <unordered_set>
+#include<set>
+
+using namespace std;
+
+map<string,int>stoi;
+vector<vector<int>>g;
+vector<vector<int>>child;
+vector<int>in;
+queue<int>q;
+
+void solve(){
+	
+	while(!q.empty()){
+		int now=q.front();
+		q.pop();
+		for(int i=0;i<g[now].size();i++){
+			int there=g[now][i];
+			in[there]--;
+			if(in[there]==0){
+				
+				q.push(there);
+				child[now].push_back(there);
+			}
+			
+		}
+		
+		
+	}
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	map<string,int>stoi;
+	int n;
+	cin>>n;
+	g=vector<vector<int>>(n+1);
+	child=vector<vector<int>>(n+1);
+	in=vector<int>(n+1);
+	for(int i=1;i<=n;i++){
+		string temp;
+		cin>>temp;
+		stoi[temp]=i;
+	}
+	
+	int m;
+	cin>>m;
+	for(int i=0;i<m;i++){
+		string a,b;
+		cin>>a>>b;
+		int na=stoi[a];
+		int nb=stoi[b];
+		
+		g[nb].push_back(na);
+		in[na]++;
+		
+	}
+	vector<int>root;
+	
+	for(int i=1;i<n+1;i++){
+		if(in[i]==0){
+			q.push(i);
+			root.push_back(i);
+		}
+	}
+	
+	cout<< root.size()<<'\n';
+	sort(root.begin(),root.end());
+	
+	for(int i=0;i<root.size();i++){
+		char temp='a'+root[i]-1;
+		
+		cout<<temp<<' ';
+	}
+	cout<<'\n';
+	
+	solve();
+	
+	
+	for(int i=1;i<n+1;i++){
+		char temp='a'+i-1;
+		cout<<temp<<' ';
+		sort(child[i].begin(),child[i].end());
+		cout<<child[i].size()<<' ';
+		for(int j=0;j<child[i].size();j++){
+			char temp='a'+child[i][j]-1;
+			cout<<temp<<' ';
+		}
+		cout<<'\n';
+	}
+	
+	
+}
