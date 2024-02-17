a19e4dbbf4a45a58c13db87a845b06196edfae0e
abm12one <87717167+abm12one@users.noreply.github.com>
2024-02-17 21:26:03 +0900

    Title: 크기 비교 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240217/크기 비교 3/size-comparison-3.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<limits.h>
+#include<cassert>
+//#include<unordered_map>
+//#include<map>
+#include <unordered_set>
+#include<set>
+
+using namespace std;
+queue<int>q;
+vector<int>visit;
+vector<int>in;
+vector<vector<int>>map;
+int n,m;
+vector<int>ans;
+void solve(){
+	
+	while(!q.empty()){
+		int now=q.front();
+		ans.push_back(now);
+		q.pop();
+		for(int i=0;i<map[now].size();i++){
+			int there=map[now][i];
+			in[there]--;
+			if(in[there]==0){
+				q.push(there);
+			}
+			
+		}
+		
+	}
+	
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin>>n>>m;
+	visit=vector<int>(n+1);
+	in=vector<int>(n+1);
+	map=vector<vector<int>>(n+1);
+	for(int i=0;i<m;i++){
+		int a,b;
+		cin>>a>>b;
+		in[b]++;
+		map[a].push_back(b);
+	}
+	for(int i=0;i<map.size();i++){
+		sort(map[i].begin(),map[i].end());
+	}
+	
+	for(int i=1;i<n+1;i++){
+		if(in[i]==0){
+			q.push(i);
+		}
+	}
+	
+	solve();
+	
+	for(int i=0;i<ans.size();i++){
+		cout<<ans[i]<<' ';
+	}
+}
