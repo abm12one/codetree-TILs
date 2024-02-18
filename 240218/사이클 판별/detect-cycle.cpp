1e74a2514496d5f451759f11839e41fb8ed74733
abm12one <87717167+abm12one@users.noreply.github.com>
2024-02-18 16:34:51 +0900

    Title: 사이클 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240218/사이클 판별/detect-cycle.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<limits.h>
+#include<cassert>
+//#include<unordered_map>
+//#include<map>
+#include <unordered_set>
+#include<set>
+using namespace std;
+vector<vector<int>>map;
+vector<int>visit;
+queue<int>q;
+vector<int>ans;
+vector<int>in;
+int n;
+int solve(){
+	int num=0;
+	while(!q.empty()){
+		int now=q.front();
+		num++;
+		cout<<now<<'\n';
+		q.pop();
+		for(int i=0;i<map[now].size();i++){
+			int there=map[now][i];
+			in[there]--;
+			if(in[there]==0){
+				q.push(there);
+				
+			}
+			
+		}
+		
+		
+	}
+	return num;
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int m;
+	cin>>n>>m;
+	map=vector<vector<int>>(n+1);
+	in=vector<int>(n+1);
+	for(int i=0;i<m;i++){
+		int a,b;
+		cin>>a>>b;
+		map[a].push_back(b);
+		in[b]++;
+	}
+	
+	for(int i=1;i<n+1;i++){
+		if(in[i]==0){
+			q.push(i);
+		}
+	}
+	
+	if(solve()==n){
+		cout<<"Not Exists"<<'\n';	
+	}
+	else{
+		cout<<"Exists"<<'\n';
+	}
+	
+}
