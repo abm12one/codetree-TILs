2fe981daed90e6da8817c290e0afb8f99da96c95
abm12one <87717167+abm12one@users.noreply.github.com>
2024-02-19 22:41:56 +0900

    Title: 갈 수 있는 경우의 수, Time: 28ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240219/갈 수 있는 경우의 수/possible-path-of-travel.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<limits.h>
+#include<cassert>
+//#include<unordered_map>
+//#include<map>
+//#include <unordered_set>
+//#include<set>
+
+using namespace std;
+
+vector<int>cache;
+vector<vector<int>>pre;
+
+int dp(int now){
+	
+	int &ret=cache[now];
+	
+	if(ret!=-1){
+		return ret;
+	}
+	
+	ret=0;
+	
+	for(int i=0;i<pre[now].size();i++){
+		int p=pre[now][i];
+		ret+=dp(p);
+		ret%=1000000007;
+	}
+	
+	if(ret==0){
+		return ret=1;
+	}
+	
+	return ret;
+	
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n,m;
+	cin>>n>>m;
+	cache=vector<int>(n+1,-1);
+	pre=vector<vector<int>>(n+1);
+	for(int i=0;i<m;i++){
+		int a,b;
+		cin>>a>>b;
+		pre[b].push_back(a);
+		
+	}
+	
+	cout<<dp(n)<<'\n';
+	
+}
