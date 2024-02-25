c1ca014c7f0afcf593a6ea70f6ee9921038390a2
abm12one <87717167+abm12one@users.noreply.github.com>
2024-02-25 22:42:01 +0900

    Title: 최대로 방문 가능한 경로, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240225/최대로 방문 가능한 경로/maximum-visitable-route.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<limits.h>
+#include<cassert>
+//#include<unordered_map>
+//#include<map>
+//#include <unordered_set>
+//#include<set>
+
+using namespace std;
+
+vector<vector<int>>map;
+vector<int>cache;
+vector<int>arr;
+int dp(int n){
+	
+	int &ret=cache[n];
+	if(ret!=-1){
+		return ret;
+	}
+	ret=-1;
+	
+	int mmax=-1;
+	for(int i=0;i<map[n].size();i++){
+		
+		int p=map[n][i];
+		if(dp(p)>mmax){
+			mmax=dp(p);
+			arr[n]=p;
+		}
+		
+	}
+	if(mmax==-1){
+		return ret;
+	}
+	return ret+1;
+	
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n,m;
+	cin>>n>>m;
+	map=vector<vector<int>>(n+1);
+	cache=vector<int>(n+1,-1);
+	arr=vector<int>(n+1);
+	
+	for(int i=0;i<m;i++){
+		int a,b;
+		cin>>a>>b;
+		map[b].push_back(a);
+		
+	}
+	
+	for(int i=0;i<m;i++){
+		sort(map[i].begin(),map[i].end());
+	}
+	cache[1]=1;
+	
+	if(dp(n)==-1){
+		cout<<-1<<'\n';
+		return 0;
+	}
+	/*
+	for(int i=0;i<n+1;i++){
+		cout<<arr[i]<<'\n';
+	}
+	for(int i=0;i<n+1;i++){
+		cout<<cache[i]<<'\n';
+	}
+	*/
+	int now=n;
+	vector<int>r;
+	
+	while(1){
+		if(arr[now]==0)break;
+		r.push_back(now);
+		now=arr[now];
+	}
+	r.push_back(1);
+	reverse(r.begin(),r.end());
+	
+	for(int i=0;i<r.size();i++){
+		cout<<r[i]<<' ';
+	}	
+	
+
+	
+}
