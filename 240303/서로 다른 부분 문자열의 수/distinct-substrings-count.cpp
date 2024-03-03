f0ae5d63c3d0055f21fd94bcbaed03db5670f07a
abm12one <87717167+abm12one@users.noreply.github.com>
2024-03-03 22:56:38 +0900

    Title: 서로 다른 부분 문자열의 수, Time: 2201ms, Memory: 0MB, Status: Passed - CodeTree

--- 240303/서로 다른 부분 문자열의 수/distinct-substrings-count.cpp
+++ 240303/서로 다른 부분 문자열의 수/distinct-substrings-count.cpp
@@ -20,11 +20,11 @@ using namespace std;
 string s;
 
 int n;
-int ans=0;
+long long ans=0;
 // 2개의 polynomial rolling 해싱을 위한 p, m 값을 정의합니다.
 int p[2] = {31, 37};
 int m[2] = {int(1e9) + 7, int(1e9) + 9};
-
+vector<vector<long long>>dp;
 // p^i, 값을 m으로 나눈 나머지를 관리합니다.
 long long p_pow[2][MAX_N + 1];
 
@@ -39,15 +39,21 @@ void Exists(int l) {
     // p_pow 값을 계산합니다.
    
     long long h[2] = {};
-    for(int k = 0; k < 2; k++) {
-        for(int i = 0; i < l; i++)
-            h[k] = (h[k] + ToInt(s[i]) * p_pow[k][l - 1 - i]) % m[k];
+	
+	if(l==1){
+            for(int k=0; k<2; k++){
+                h[k] =  ToInt(s[0]) %m [k]; 
+                dp[k][1] = h[k]; 
+            }
+        }
+    else{
+        for(int k=0; k<2; k++){
+        	h[k] = (dp[k][l-1] * p[k] + ToInt(s[l-1])) % m[k]; 
+            dp[k][l] = h[k]; 
+        } 
     }
-
-    // set에 넣어줍니다.
-    if(hs.find(make_pair(h[0], h[1])) == hs.end()){
-	   hs.insert(make_pair(h[0], h[1]));
-    }
+	
+	hs.insert(make_pair(h[0], h[1]));
 	
     // [i, i + l - 1] 구간을 부분문자열로 하는 경우를 전부 탐색합니다.
     for(int i = 1; i <= n - l; i++) {
@@ -58,10 +64,6 @@ void Exists(int l) {
             if(h[k] < 0)
                 h[k] += m[k];
         }
-
-        // 이미 동일한 해싱값이 존재한다면
-        // 2번 이상 등장하는 것입니다.
-        if(hs.find(make_pair(h[0], h[1])) != hs.end()) continue;
 
         hs.insert(make_pair(h[0], h[1]));
     }
@@ -76,6 +78,7 @@ int main() {
     cin >> s;
 
     n = (int) s.size();
+	dp=vector<vector<long long>>(2,vector<long long>(n+1));
 	for(int k = 0; k < 2; k++) {
         // p_pow[i] = p^i % m
         p_pow[k][0] = 1;
