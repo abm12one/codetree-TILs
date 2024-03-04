d40bb291b6cac6414ca29f9e28ea22adbd567758
abm12one <87717167+abm12one@users.noreply.github.com>
2024-03-04 22:59:41 +0900

    Title: 문자열 일치 횟수, Time: 11ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240304/문자열 일치 횟수/string-match-count.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<limits.h>
+#include<cassert>
+#include<unordered_map>
+//#include< map>
+#include<unordered_set>
+#include<set>
+#define MAX_M 100000
+
+using namespace std;
+
+string text,pattern;
+
+int n;
+
+vector<int>f;
+
+int main() {
+	
+	cin>>n;
+	cin>>text;
+	cin>>pattern;
+	
+	int tn=2*text.size();
+	int pn=pattern.size();
+	
+	text=text+text;
+	text='#'+text;
+	
+	pattern='#'+pattern;
+	
+	f=vector<int>(pn+1);
+	
+	f[0]=-1;
+	
+	for(int i=1;i<=pn;i++){
+		
+		int j=f[i-1];
+		
+		while(j>=0&&pattern[i]!=pattern[j+1]){
+			j=f[j];
+		}
+		
+		f[i]=j+1;
+		
+	}
+	
+	int j=0;
+	int ans=0;
+	for(int i=1;i<=tn;i++){
+		
+		while(j>=0&&text[i]!=pattern[j+1]){
+			j=f[j];
+		}
+		
+		j++;
+		
+		if(j==pn){
+			if(i!=pn){
+				ans++;
+			}
+			j=f[j];
+		}
+		
+		
+	}
+	cout<<ans<<'\n';
+	
+	
+	
+}
