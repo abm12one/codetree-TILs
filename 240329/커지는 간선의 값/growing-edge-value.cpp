59ba63326b01b12c343ab48b1bc4bb4de72fc898
abm12one <87717167+abm12one@users.noreply.github.com>
2024-03-29 22:11:57 +0900

    Title: 커지는 간선의 값, Time: 147ms, Memory: 5MB, Status: Wrong Answer - CodeTree

---
+++ 240329/커지는 간선의 값/growing-edge-value.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<limits.h>
+#include<cassert>
+//#include<unordered_map>
+//#include<map>
+#include<unordered_set>
+#include<set>
+
+#define MAX_K 100
+
+using namespace std;
+
+int n,m,k;
+
+vector<vector<pair<int,int>>>map;
+vector<int>dis;
+vector<int>visit;
+priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+
+int main() {
+
+    cin>>n>>m>>k;
+    map=vector<vector<pair<int,int>>>(n+1);
+    dis=vector<int>(n+1,1e9);
+    visit=vector<int>(n+1,0);
+    for(int i=0;i<m;i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        map[a].push_back(make_pair(b,c));
+        map[b].push_back(make_pair(a,c));
+
+    }
+
+    pq.push(make_pair(0,1));
+    dis[1]=0;
+    int ans=0;
+    while(!pq.empty()){
+        int d,now;
+        tie(d,now)=pq.top();
+        pq.pop();
+
+        if(d>dis[now])continue;
+        visit[now]=1;
+        ans+=dis[now];
+
+        for(int i=0;i<map[now].size();i++){
+            int next,nd;
+            tie(next,nd)=map[now][i];
+            if(visit[next])continue;
+            if(dis[next]>nd){
+                dis[next]=nd;
+                pq.push(make_pair(nd,next));
+            }
+        }
+
+
+
+    }
+    int sum=0;
+    for(int i=0;i<n-1;i++){
+        sum+=k*i;
+    }
+    cout<<sum+ans;
+
+    
+
+
+
+ 
+}
