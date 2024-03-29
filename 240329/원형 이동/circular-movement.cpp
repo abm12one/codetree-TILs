9391b5f28ca569bf35477dedaec209225f6c122c
abm12one <87717167+abm12one@users.noreply.github.com>
2024-03-29 20:24:04 +0900

    Title: 원형 이동, Time: 230ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

--- 240329/원형 이동/circular-movement.cpp
+++ 240329/원형 이동/circular-movement.cpp
@@ -15,7 +15,7 @@
 
 using namespace std;
 
-priority_queue<pair<int,int>>pq;
+priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
 vector<int>visit;
 vector<int>dis;
 vector<vector<int>>map;
@@ -27,9 +27,9 @@ int main() {
     int n,m,k;
     cin>>n>>m>>k;
 
-    dis=vector<int>(n+1,1e9);
+    dis=vector<int>(n+1,1e9+5);
     visit=vector<int>(n+1,0);
-    map=vector<vector<int>>(n+1,vector<int>(n+1,1e9));
+    map=vector<vector<int>>(n+1,vector<int>(n+1,-1));
 
     for(int i=1;i<=n;i++){
         int t;
@@ -42,22 +42,25 @@ int main() {
         if(i==n){
             map[i][1]=0;
             map[1][i]=0;
-            continue;
+            
         }
-        map[i][i+1]=0;
-        map[i+1][i]=0;
+        else{
+            map[i][i+1]=0;
+            map[i+1][i]=0;
+        }
+        
     }
 
     for(int i=0;i<m;i++){
         int a,b;
         cin>>a>>b;
-        map[a][b]=k+1;
-        map[b][a]=k+1;
+        map[a][b]=-1;
+        map[b][a]=-1;
     }
 
     dis[0]=0;
     pq.push(make_pair(0,0));
-    long long ans=0;
+    int ans=0;
 
     while(!pq.empty()){
         
@@ -67,15 +70,20 @@ int main() {
 
         if(d>dis[now])continue;
         if(visit[now])continue;
+
         visit[now]=1;
         ans+=d;
+        //cout<<d<<' '<<now<<'\n';
+        
         if(ans>k){
             cout<<0;
             return 0;
         }
+        
 
         for(int i=0;i<n+1;i++){
             if(visit[i])continue;
+            if(map[now][i]==-1)continue;
             if(map[now][i]<dis[i]){
                 dis[i]=map[now][i];
                 pq.push(make_pair(map[now][i],i));
@@ -85,6 +93,7 @@ int main() {
 
 
     }
+    //cout<<ans<<'\n';
     if(ans>k){
         cout<<0;
     }
