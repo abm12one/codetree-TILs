61a20c999986eb3d99d0433740cb2f686cb80755
abm12one <87717167+abm12one@users.noreply.github.com>
2024-04-01 22:18:36 +0900

    Title: 기사 출정식, Time: 258ms, Memory: 9MB, Status: Passed - CodeTree

--- 240401/기사 출정식/knights-commencement.cpp
+++ 240401/기사 출정식/knights-commencement.cpp
@@ -1,71 +1,66 @@
 #include <iostream>
-#include <vector>
-
-#define MAX_N 250000
-#define MAX_K 100
+#include <unordered_map>
 
 using namespace std;
-vector<int>arr;
 
-struct node{
-    int data;
-    node*prev;
-    node*next;
-    node(int data):data(data),prev(nullptr),next(nullptr){};
+#define MAXN 100002
+
+struct Node {
+    int val;
+    Node *prev, *next;
+
+    Node(int val) : val(val), prev(nullptr), next(nullptr) {}
 };
 
-node* nodes[100000001];
+// 기사들을 관리해줄 배열입니다.
+Node *nodes[MAXN];
 
-void connect(node *s,node*e){
-    if(s!=nullptr){
-        s->next=e;
-    }
-    if(e!=nullptr){
-        e->prev=s;
-    }
+// 기사들의 번호의 범위가 1 ~ 10억이기 때문에, map으로 기사들의 번호들을 관리해줍니다.
+unordered_map<int, int> nodeId;
 
+// 두 기사들을 연결해줍니다.
+void connect(Node *s, Node *e) {
+    if (nullptr != s) s->next = e;
+    if (nullptr != e) e->prev = s;
 }
 
-void pr(node* now){
-    cout<<now->next->data<<" ";
-    cout<<now->prev->data<<'\n';
-}
-
-void pop(node *s){
-    connect(s->prev,s->next);
-    s->prev=nullptr;
-    s->next=nullptr;
-
+// 해당 기사를 자리에서 없애줍니다.
+void pop(Node *u) {
+    connect(u->prev, u->next);
+    u->prev = u->next = nullptr;
 }
 
 int main() {
+    // 입력
+    int n, m;
+    cin >> n >> m;
 
-    int n,q;
-    cin>>n>>q;
+    // 원탁이기 때문에 연결 리스트가 원형으로 이어져 있음에 유의합니다.
+    // 첫번째 기사를 먼저 입력받고, 이후에는 이전 기사와 현재 기사를 연결해줍니다.
+    int knightNum;
+    cin >> knightNum;
 
-    arr=vector<int>(n+1);
+    // map의 기사의 번호와 배열의 인덱스를 매칭시켜줍니다.
+    nodeId[knightNum] = 1;
+    nodes[1] = new Node(knightNum);
+    for (int i = 2; i <= n; i++) {
+        cin >> knightNum;
+        nodeId[knightNum] = i;
+        nodes[i] = new Node(knightNum);
+        connect(nodes[i - 1], nodes[i]);
 
-    for(int i=1;i<=n;i++){
-        cin>>arr[i];
-        nodes[arr[i]]=new node(arr[i]);
+        // 마지막 기사를 입력받았다면, 첫번째 기사와 마지막 기사를 연결해줍니다.
+        if (i == n) connect(nodes[n], nodes[1]);
     }
 
-    for(int i=1;i<n;i++){
-        connect(nodes[arr[i]],nodes[arr[i+1]]);
+    // 왕이 기사들의 번호를 부를 때마다
+    // 해당 기사의 왼쪽과 오른쪽에 있는 기사들의 번호를 출력합니다.
+    while (m--) {
+        // 기사들의 번호를 입력받아 해당 기사의 배열에서의 인덱스를 찾아 연산을 진행합니다.
+        cin >> knightNum;
+        cout << nodes[nodeId[knightNum]]->next->val << " " << nodes[nodeId[knightNum]]->prev->val << "\n";
+
+        // 이름이 불렸다면 해당 기사를 자리에서 없애줍니다.
+        pop(nodes[nodeId[knightNum]]);
     }
-
-    connect(nodes[arr[n]],nodes[arr[1]]);
-
-    for(int i=0;i<q;i++){
-        int t;
-        cin>>t;
-        pr(nodes[t]);
-        pop(nodes[t]);
-    }
-
-    
-
-    
-    
-
 }
