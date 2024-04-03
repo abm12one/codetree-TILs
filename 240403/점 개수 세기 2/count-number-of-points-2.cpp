69f6b5cdb719a4543e47b22a4e44ea7c71b442f5
abm12one <87717167+abm12one@users.noreply.github.com>
2024-04-03 22:54:30 +0900

    Title: 점 개수 세기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240403/점 개수 세기 2/count-number-of-points-2.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <set>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+set<int>s;
+unordered_map<int,int>m;
+vector<pair<int,int>>arr;
+vector<vector<int>>gp;
+vector<vector<int>>psum;
+
+int n,q;
+
+int low(int x){
+    auto it=s.lower_bound(x);
+    if(it==s.end()){
+        return s.size()+1;
+    }
+    return m[*it];
+}
+
+int upp(int x){
+    auto it=s.upper_bound(x);
+    if(it==s.begin()){
+        return 0;
+    }
+    it--;
+    return m[*it];
+}
+
+int sol(int y1,int x1,int y2,int x2){
+    int ly=low(y1);
+    int lx=low(x1);
+    int uy=upp(y2);
+    int ux=upp(x2);
+
+    return psum[uy][ux]-psum[uy][lx]-psum[ly][uy]+psum[ly][lx];
+
+
+}
+
+int main() {
+    
+    cin>>n>>q;
+    
+    
+    for(int i=0;i<n;i++){
+        int a,b;
+        cin>>a>>b;
+        arr.push_back(make_pair(a,b));
+
+        s.insert(a);
+        s.insert(b);
+    }
+    
+    int num=1;
+    for(auto it=s.begin();it!=s.end();it++){
+        m[*it]=num++;
+    }
+    gp=vector<vector<int>>(num+1,vector<int>(num+1));
+    psum=vector<vector<int>>(num+1,vector<int>(num+1));
+    for(int i=0;i<n;i++){
+        int a,b;
+        tie(a,b)=arr[i];
+        int y=m[a];
+        int x=m[b];
+        gp[y][x]++;
+    }
+
+   
+    
+    for(int i=1;i<=num;i++){
+        for(int j=1;j<=num;j++){
+            psum[i][j]=psum[i][j-1]+psum[i-1][j]-psum[i-1][j-1]+gp[i][j];
+        }
+    }
+    
+
+    for(int i=0;i<q;i++){
+        int y1,x1,y2,x2;
+        cin>>y1>>x1>>y2>>x2;
+        cout<<sol(y1,x1,y2,x2)<<'\n';
+
+    }
+
+
+
+
+
+
+}
