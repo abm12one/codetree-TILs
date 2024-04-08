4d43e5a862cf65274a0d3a1882cfab2979936200
abm12one <87717167+abm12one@users.noreply.github.com>
2024-04-08 19:59:08 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 1082ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240408/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>>map;
+vector<vector<int>>ans;
+int n,m;
+int dy[4]={0,1,0,-1};
+int dx[4]={1,0,-1,0};
+
+int range(int y,int x){
+    if(x<0||x>=m)return 0;
+    if(y<0||y>=n)return 0;
+    return 1;
+
+}
+
+int main() {
+
+    cin>>n>>m;
+
+    map=vector<vector<int>>(n,vector<int>(n));
+
+    int y=0;
+    int x=0;
+    map[0][0]=1;
+
+    int d=0;
+
+    while(1){
+
+        int ny=y+dy[d];
+        int nx=x+dx[d];
+
+        if(!range(ny,nx)||map[ny][nx]!=0){
+            d=(d+1)%4;
+            continue;
+        }
+
+        map[ny][nx]=map[y][x]+1;
+        if(map[ny][nx]==n*m)break;
+        y=ny;
+        x=nx;
+        
+
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<map[i][j]<<' ';
+        }
+        cout<<'\n';
+
+    }
+
+    
+
+ 
+    
+}
