aaa05df277d31fba991ba02dd0c9e13a1daccc41
abm12one <87717167+abm12one@users.noreply.github.com>
2024-04-12 22:58:42 +0900

    Title: 개인정보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240412/개인정보/personal-info.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+using namespace std;
+
+class node{
+    public:
+        string name;
+        int height;
+        double weight;
+        
+
+        node(string a,int b,double c){
+            name=a;
+            height=b;
+            weight=c;
+            
+        }
+
+};
+
+bool cmp(node a,node b){
+    return a.name<b.name;
+}
+
+bool cmp2(node a,node b){
+    return a.height>b.height;
+}
+
+vector<node>arr;
+
+int main() {
+
+    int n=5;
+
+    for(int i=0;i<n;i++){
+        string a;
+        int b;
+        double c;
+
+        cin>>a>>b>>c;
+        arr.push_back(node(a,b,c));
+
+
+
+    }
+    cout<<"name"<<'\n';
+    sort(arr.begin(),arr.end(),cmp);
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i].name<<' '<<arr[i].height<<' '<<arr[i].weight<<'\n';
+    }
+    cout<<'\n';
+    cout<<"height"<<'\n';
+
+    sort(arr.begin(),arr.end(),cmp2);
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i].name<<' '<<arr[i].height<<' '<<arr[i].weight<<'\n';
+    }
+
+    
+
+
+    
+}
