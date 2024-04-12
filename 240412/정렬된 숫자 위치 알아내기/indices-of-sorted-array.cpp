153ea434192492762da0b6371eedb20f34e6554a
abm12one <87717167+abm12one@users.noreply.github.com>
2024-04-12 23:21:16 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 6ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240412/정렬된 숫자 위치 알아내기/indices-of-sorted-array.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+using namespace std;
+
+class node{
+    public:
+        int id;
+        int num;
+        
+
+        node(int a,int b){
+            id=a;
+            num=b;
+            
+        }
+
+};
+
+bool cmp(node a,node b){
+    if(a.num==b.num){
+        return a.id<b.id;
+    }
+    
+    return a.num<b.num;
+}
+
+vector<node>arr;
+vector<int>ans;
+
+int main() {
+
+    int n;
+    cin>>n;
+    ans=vector<int>(n);
+
+    for(int i=0;i<n;i++){
+        int t;
+        cin>>t;
+
+        
+        arr.push_back(node(i+1,t));
+
+
+
+    }
+   
+    sort(arr.begin(),arr.end(),cmp);
+
+    for(int i=0;i<n;i++){
+        int tid=arr[i].id;
+        ans[tid]=i+1;
+
+    }
+    for(int i=1;i<=n;i++){
+        cout<<ans[i]<<' ';
+    }
+    
+    
+
+
+    
+}
