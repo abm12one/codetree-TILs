bfeb4ee8ea8fee63c288379b7b962b5807770710
abm12one <87717167+abm12one@users.noreply.github.com>
2024-04-14 23:13:54 +0900

    Title: 1차원 폭발 게임, Time: 1014ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240414/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+using namespace std;
+vector<int>arr;
+int sn;
+
+void sol(int a,int b){
+    //cout<<a<<' '<<b<<"\n";
+    for(int i=a;i<=b;i++){
+        arr[i]=0;
+    }
+
+    for(int i=b+1;i<=sn;i++){
+        arr[i+a-b-1]=arr[i];
+    }
+    sn-=(b-a+1);
+    //cout<<sn<<'\n';
+
+}
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    arr=vector<int>(n+1);
+    for(int i=1;i<=n;i++){
+        cin>>arr[i];
+    }
+    sn=n;
+
+    while(1){
+        int s=-1;
+        int e=-1;
+        int temp=arr[1];
+        s=1;
+
+        int a=0;
+        int b=0;
+
+        int cnt=1;
+        for(int i=2;i<=sn;i++){
+            if(arr[i]!=temp){
+                if(cnt>=m){
+                    e=i-1;
+                    a=s;
+                    b=e;
+                    s=i;
+                }
+                e=i-1;
+                s=i;
+                cnt=1;
+                temp=arr[i];
+                
+            }
+            else if(arr[i]==temp){
+                cnt++;
+            }
+            
+        }
+
+        if(cnt>=m){
+            a=s;
+            b=sn;
+        }
+
+        if(a==0&&b==0){
+            break;
+        }
+        
+
+        sol(a,b);
+
+        
+
+    }
+
+    
+    cout<<sn<<'\n';
+    
+    
+    for(int i=1;i<=sn;i++){
+        cout<<arr[i]<<'\n';
+    }
+    
+
+
+    
+    
+
+}
