e1d5603957e66935f822e737a86da3f7a7ae809c
abm12one <87717167+abm12one@users.noreply.github.com>
2024-04-15 19:19:55 +0900

    Title: 단 한 번의 2048 시도, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240415/단 한 번의 2048 시도/one-trial-of-2048-game.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+using namespace std;
+int n=4;
+vector<vector<int>>map;
+
+
+void shiftr(int y){
+    vector<int>arr(4);
+    for(int i=0;i<4;i++){
+        arr[i]=map[y][i];
+    }
+    for(int i=0;i<4;i++){
+        map[y][i]=0;
+    }
+    for(int i=3;i>=0;i--){
+        if(arr[i]==arr[i-1]){
+            arr[i]+=arr[i-1];
+            arr[i-1]=0;
+            i--;
+        }
+
+    }
+    int s=3;
+    for(int i=3;i>=0;i--){
+        if(arr[i]==0)continue;
+        map[y][s]=arr[i];
+        s--;
+    }
+}
+void shiftl(int y){
+    vector<int>arr(4);
+    for(int i=0;i<4;i++){
+        arr[i]=map[y][i];
+    }
+    for(int i=0;i<4;i++){
+        map[y][i]=0;
+    }
+    for(int i=0;i<3;i++){
+        if(arr[i]==arr[i+1]){
+            arr[i]+=arr[i+1];
+            arr[i+1]=0;
+            i++;
+        }
+
+    }
+    int s=0;
+    for(int i=0;i<4;i++){
+        if(arr[i]==0)continue;
+        map[y][s]=arr[i];
+        s++;
+    }
+
+
+
+}
+void shiftu(int x){
+    vector<int>arr(4);
+    for(int i=0;i<4;i++){
+        arr[i]=map[i][x];
+    }
+    for(int i=0;i<4;i++){
+        map[i][x]=0;
+    }
+    for(int i=0;i<3;i++){
+        if(arr[i]==arr[i+1]){
+            arr[i]+=arr[i+1];
+            arr[i+1]=0;
+            i++;
+        }
+
+    }
+    int s=0;
+    for(int i=0;i<4;i++){
+        if(arr[i]==0)continue;
+        map[s][x]=arr[i];
+        s++;
+    }
+}
+void shiftd(int x){
+    vector<int>arr(4);
+    for(int i=0;i<4;i++){
+        arr[i]=map[i][x];
+    }
+    for(int i=0;i<4;i++){
+        map[i][x]=0;
+    }
+    for(int i=3;i>=0;i--){
+        if(arr[i]==arr[i-1]){
+            arr[i]+=arr[i-1];
+            arr[i-1]=0;
+            i--;
+        }
+
+    }
+    int s=3;
+    for(int i=3;i>=0;i--){
+        if(arr[i]==0)continue;
+        map[s][x]=arr[i];
+        s--;
+    }
+
+}
+int main() {
+
+    map=vector<vector<int>>(4,vector<int>(4));
+    
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cin>>map[i][j];
+        }
+    }
+
+    char c;
+    cin>>c;
+    if(c=='R'){
+        for(int i=0;i<4;i++){
+            shiftr(i);
+        }
+    }
+    else if(c=='L'){
+        for(int i=0;i<4;i++){
+            shiftl(i);
+        }
+    }
+    else if(c=='U'){
+        for(int i=0;i<4;i++){
+            shiftu(i);
+        }
+    }
+    else if(c=='D'){
+        for(int i=0;i<4;i++){
+            shiftd(i);
+        }
+    }
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cout<<map[i][j]<<' ';
+        }
+        cout<<'\n';
+    }
+
+    
+    
+
+
+    
+    
+
+}
