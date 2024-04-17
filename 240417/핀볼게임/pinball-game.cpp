46f15d5d80ecfcb49d9e0cc8e6c9c7088bab37ad
abm12one <87717167+abm12one@users.noreply.github.com>
2024-04-17 23:24:32 +0900

    Title: 핀볼게임, Time: 1077ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240417/핀볼게임/pinball-game.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <list>
+#include <unordered_set>
+
+using namespace std;
+
+int n;
+int ans=0;
+int dy[4]={-1,0,1,0};
+int dx[4]={0,1,0,-1};
+vector<vector<int>>map;
+int range(int y,int x){
+    if(y<1||y>n) return 0;
+    if(x<1||x>n) return 0;
+    return 1;
+}
+int t;
+int changedir(int d,int mirror){
+    if(mirror==1){
+        return (5-d)%4;
+    }
+    else if(mirror==2){
+        return 3-d;
+    }
+}
+
+void dfs(int y,int x,int d){
+    t++;
+    int ny=y+dy[d];
+    int nx=x+dx[d];
+    if(!range(ny,nx))return;
+    if(map[ny][nx]==0){
+        dfs(ny,nx,d);
+    }
+    else{
+        int ndir=changedir(d,map[ny][nx]);
+        dfs(ny,nx,ndir);
+    }
+}
+
+void sol(int y,int x,int d){
+    t=1;
+    dfs(y,x,d);
+    ans=max(ans,t);
+}
+
+
+int main() {
+
+   
+    cin>>n;
+    map=vector<vector<int>>(n+1,vector<int>(n+1));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cin>>map[i][j];
+        }
+    }
+
+    for(int i=1;i<=n;i++){
+        sol(i,1,1);
+        sol(i,n,3);
+        sol(1,i,2);
+        sol(n,i,0);
+    }
+    cout<<ans;
+
+
+    
+
+    
+    
+
+}
