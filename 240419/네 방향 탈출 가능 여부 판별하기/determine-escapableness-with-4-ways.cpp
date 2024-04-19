85b1896af0cacb15b62aae3d310bfc2b7092a4b9
abm12one <87717167+abm12one@users.noreply.github.com>
2024-04-19 21:32:00 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240419/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+using namespace std;
+int n,m;
+vector<vector<int>>map;
+vector<vector<int>>visit;
+queue<pair<int,int>>q;
+
+int dy[4]={1,0,-1,0};
+int dx[4]={0,1,0,-1};
+
+int range(int y,int x){
+    if(x<1||x>m)return 0;
+    if(y<1||y>n)return 0;
+    return 1;
+
+}
+
+void bfs(int r,int c){
+    q.push(make_pair(r,c));
+    visit[r][c]=1;
+    while(!q.empty()){
+        int y,x;
+        tie(y,x)=q.front();
+        q.pop();
+        for(int i=0;i<4;i++){
+            int ny=y+dy[i];
+            int nx=x+dx[i];
+            if(!range(ny,nx))continue;
+            if(map[ny][nx]==1&&visit[ny][nx]==0){
+                q.push(make_pair(ny,nx));
+                visit[ny][nx]=1;
+            }
+        }
+
+    }
+    return;
+
+}
+
+int main() {
+
+    cin>>n>>m;
+    map=vector<vector<int>>(n+1,vector<int>(m+1));
+    visit=vector<vector<int>>(n+1,vector<int>(m+1));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            cin>>map[i][j];
+        }
+    }
+    bfs(1,1);
+    /*
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            cout<<visit[i][j]<<' ';
+        }
+        cout<<"\n";
+    }
+    cout<<"\n";
+    */
+    if(visit[n][m]==1){
+        cout<<1;
+    }
+    else{
+        cout<<0;
+    }
+
+    
+
+
+}
