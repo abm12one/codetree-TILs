19bfc236753bd38909228148e486b3b6cf3e0a28
abm12one <87717167+abm12one@users.noreply.github.com>
2024-04-22 18:57:34 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 24ms, Memory: 7MB, Status: Passed - CodeTree

--- 240422/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
+++ 240422/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
@@ -18,14 +18,7 @@ int main() {
         int now=q.front();
         q.pop();
         //
-        if(now+1<=n+1&&visit[now+1]==-1){
-            q.push(now+1);
-            visit[now+1]=visit[now]+1;
-        }
-        if(now-1>0&&visit[now+1]==-1){
-            q.push(now-1);
-            visit[now-1]=visit[now]+1;
-        }
+        
         if(now%2==0&&now/2>0&&visit[now/2]==-1){
             q.push(now/2);
             visit[now/2]=visit[now]+1;
@@ -34,8 +27,17 @@ int main() {
             q.push(now/3);
             visit[now/3]=visit[now]+1;
         }
+        if(now+1<=n+1&&visit[now+1]==-1){
+            q.push(now+1);
+            visit[now+1]=visit[now]+1;
+        }
+        if(now-1>0&&visit[now-1]==-1){
+            q.push(now-1);
+            visit[now-1]=visit[now]+1;
+        }
 
 
     }
-    cout<<visit[1];
+    
+    cout<<visit[1]<<"\n";
 }
