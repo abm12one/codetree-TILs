1e206fdc44a3161935f181034ac50a7432aedbf9
abm12one <87717167+abm12one@users.noreply.github.com>
2024-04-22 18:53:28 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240422/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
+++ 240422/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
@@ -1,33 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
-int n;
+
 vector<int>arr;
-void sol(){
-
-    for(int i=2;i<=3*n+1;i++){
-        arr[i]=arr[i-1]+1;
-        if(i%2==0){
-            arr[i]=min(arr[i],arr[i/2]+1);
-        }
-        if(i%3==0){
-            arr[i]=min(arr[i],arr[i/3]+1);
-        }
-    }
-
-    for(int i=3*n;i>=1;i--){
-        arr[i]=min(arr[i],arr[i+1]+1);
-    }
-    return;
-
-}
-
+vector<int>visit;
+int n;
 int main() {
     
     cin>>n;
-    arr=vector<int>(3*n+2);
-    arr[1]=0;
+    arr=vector<int>(n+2);
+    visit=vector<int>(n+2,-1);
+    queue<int>q;
+    q.push(n);
+    visit[n]=0;
+    while(!q.empty()){
+        int now=q.front();
+        q.pop();
+        //
+        if(now+1<n+1&&visit[now+1]==-1){
+            q.push(now+1);
+            visit[now+1]=visit[now]+1;
+        }
+        if(now-1>0&&visit[now+1]==-1){
+            q.push(now-1);
+            visit[now-1]=visit[now]+1;
+        }
+        if(now%2==0&&now/2>0&&visit[now/2]==-1){
+            q.push(now/2);
+            visit[now/2]=visit[now]+1;
+        }
+        if(now%3==0&&now/3>0&&visit[now/3]==-1){
+            q.push(now/3);
+            visit[now/3]=visit[now]+1;
+        }
 
-    sol();
-    cout<<arr[n]<<'\n';
+
+    }
+    cout<<visit[1];
 }
