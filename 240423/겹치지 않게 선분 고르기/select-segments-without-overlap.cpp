2e8c2ea3fac2120746ca02b473a00806ff3e03ea
abm12one <87717167+abm12one@users.noreply.github.com>
2024-04-23 19:18:18 +0900

    Title: 겹치지 않게 선분 고르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240423/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+using namespace std;
+int n;
+vector<int>visit;
+vector<pair<int,int>>arr;
+int ans=0;
+
+
+
+void bt(int now,int cnt){
+    ans=max(ans,cnt);
+
+    for(int i=now+1;i<n;i++){
+        int a,b;
+        tie(a,b)=arr[i];
+        if(visit[a]==0&&visit[b]==0){
+            visit[a]=1;
+            visit[b]=1;
+            bt(i,cnt+1);
+            visit[a]=0;
+            visit[b]=0;
+
+        }
+
+    }
+    return;
+
+}
+
+
+int main() {
+    
+    cin>>n;
+    visit=vector<int>(16);
+    for(int i=0;i<n;i++){
+        int a,b;
+        cin>>a>>b;
+        arr.push_back(make_pair(a,b));
+
+    }
+    sort(arr.begin(),arr.end());
+
+    bt(-1,0);
+    cout<<ans;
+
+}
