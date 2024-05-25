03707e94d96143254de124bfced427c5abe4a5e0
abm12one <87717167+abm12one@users.noreply.github.com>
2024-05-25 20:09:38 +0900

    Title: 미로 타워 디펜스, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240525/미로 타워 디펜스/maze-tower-defense.cpp
+++ 240525/미로 타워 디펜스/maze-tower-defense.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <tuple>
 using namespace std;
-
+int ans=0;
 int n,m;
 vector<vector<int>>map;
 vector<int>arr;
 vector<int>temp;
+vector<pair<int,int>>spin;
 int d,p;
 int dy[4]={0,1,0,-1};
 int dx[4]={1,0,-1,0};
@@ -16,8 +18,8 @@ int range(int y,int x){
     return 1;
 }
 
-void setting(){
-    arr=vector<int>();
+void pre(){
+    
     int y=n/2;
     int x=n/2;
     int dir=3;
@@ -38,13 +40,22 @@ void setting(){
             x=nx;
             //cout<<dir<<' '<<s<<' '<<ny<<' '<<nx<<'\n';
             if(!range(ny,nx))return;
-            if(map[ny][nx]==0)continue;
-            arr.push_back(map[ny][nx]);
+            
+            spin.push_back(make_pair(ny,nx));
             
         }
     }
 }
 
+void setting(){
+    arr=vector<int>();
+    for(int i=0;i<spin.size();i++){
+        int y,x;
+        tie(y,x)=spin[i];
+        if(map[y][x]==0)continue;
+        arr.push_back(map[y][x]);
+    }
+}
 void pr(){
     for(int i=0;i<arr.size();i++){
         cout<<arr[i]<<' ';
@@ -72,6 +83,7 @@ void remove(int d,int p){
     for(int i=0;i<p;i++){
         int ny=y+dy[d];
         int nx=x+dx[d];
+        ans+=map[ny][nx];
         map[ny][nx]=0;
         y=ny;
         x=nx;
@@ -79,38 +91,49 @@ void remove(int d,int p){
 
 }
 
+void exp(){
+    temp=vector<int>();
+    int cnt=0;
+    int s=0;
+    for(int i=0;i<arr.size();i++){
+        if(arr[s]==arr[i]){
+            cnt++;
+        }
+        if(arr[s]!=arr[i]){
+            
+            temp.push_back(cnt);
+            temp.push_back(arr[s]);
+            cnt=1;
+            s=i;
+
+        }
+    }
+    
+    temp.push_back(cnt);
+    temp.push_back(arr[s]);
+    /*   
+    for(int i=0;i<temp.size();i++){
+        cout<<temp[i]<<' ';
+    }
+    cout<<'\n';
+    */
+    arr=temp;
+    return;
+}
+
 void put(){
     map=vector<vector<int>>(n,vector<int>(n));
-    int y=n/2;
-    int x=n/2;
-    int dir=3;
-    int cnt=0;
-    int now=0;
-    int s=1;
-    while(now<=arr.size()-1){
-        dir=(dir+3)%4;
-        //cout<<dir<<'\n';
-        if(cnt==2){
-            cnt=0;
-            s++;
-        }
-        cnt++;
-        for(int i=0;i<s;i++){
-            int ny=y+dy[dir];
-            int nx=x+dx[dir];
-            y=ny;
-            x=nx;
-            
-            if(!range(ny,nx))return;
-            map[ny][nx]=arr[now++];
-            
-        }
+    for(int i=0;i<arr.size();i++){
+        int y,x;
+        tie(y,x)=spin[i];
+        map[y][x]=arr[i];
     }
     return;
 }
 
 void shr(int s,int cnt){
     for(int i=0;i<cnt;i++){
+        ans+=arr[s+i];
         arr[s+i]=0;
     }
     return;
@@ -134,6 +157,10 @@ int com(){
 
         }
     }
+    if(cnt>=4){
+        shr(s,cnt);
+        f=1;
+    }
     return f;
 
 }
@@ -148,6 +175,26 @@ void compact(){
     return;
 }
 
+void sol(int d,int p){
+    remove(d,p);
+    setting();
+    //pr();
+    
+    while(true){
+        int k=com();
+        if(!k){
+            break;
+        }
+        compact();
+
+    }
+    //pr();
+    exp();
+    //pr();
+    put();
+    //pr2();
+}
+
 int main() {
     
     cin>>n>>m;
@@ -157,25 +204,13 @@ int main() {
             cin>>map[i][j];
         }
     }
+    pre();
     for(int i=0;i<m;i++){
         cin>>d>>p;
+        sol(d,p);
     }
-    
-    
-    remove(d,p);
-    setting();
-    pr();
-    
-    while(1){
-        int k=com();
-        if(k==0)return;
-    }
-    
-    compact();
-    pr();
-    put();
-    pr2();
 
+    cout<<ans;
 
 
 }
