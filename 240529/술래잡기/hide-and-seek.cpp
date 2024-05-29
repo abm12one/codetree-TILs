ce8355394f2df30a7b3da8b7cb772b05665c0e96
abm12one <87717167+abm12one@users.noreply.github.com>
2024-05-29 20:55:51 +0900

    Title: 술래잡기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240529/술래잡기/hide-and-seek.cpp
+++ 240529/술래잡기/hide-and-seek.cpp
@@ -1,65 +1,251 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+using namespace std;
 int n,m,h,k;
-vector<tuple<int,int,int>>thief
-vector<tuple<int,int,int>>trees;
-vector<vector<int>>tree;
+vector<tuple<int,int,int>>thief;
+vector<tuple<int,int,int>>temp;
+vector<vector<int>>tr;
 vector<vector<int>>map;
 vector<tuple<int,int,int>>path;
-
+vector<tuple<int,int,int>>path2;
 int range(int y,int x){
     if(y<1||y>n)return 0;
     if(x<1||x>n)return 0;
     return 1;
 }
+int dy[4]={-1,0,1,0};
+int dx[4]={0,1,0,-1};
+int d=0;
+void pr(){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<map[i][j]<<' ';
+        }
+        cout<<'\n';
+    }
+    cout<<'\n';
+    return;
+}
 
 void setting(){
-    int y=n/2;
-    int x=n/2;
+    int y=n/2+1;
+    int x=n/2+1;
     int cnt=0;
-    for(int i=1;i<=n;i++){
-        
+    int s=1;
+    
+    while(1){
         if(cnt==2){
             cnt=0;
-            break;
+            s++;
         }
-        for(int j=0;j<i-1;j++){
+        for(int j=0;j<s-1;j++){
             int ny=y+dy[d];
             int nx=x+dx[d];
             if(!range(ny,nx))return;
-            path.push_back(ny,nx,d);
+            path.push_back(make_tuple(ny,nx,d));
+            //cout<<cnt<<' '<<ny<<' '<<nx<<' '<<d<<"\n";
             y=ny;
-            x=dx;
+            x=nx;
         }
-        d=(d+1)%4;
         int ny=y+dy[d];
         int nx=x+dx[d];
-        path.push_back(ny,nx,d);
+        if(!range(ny,nx))return;
+        d=(d+1)%4;
+        path.push_back(make_tuple(ny,nx,d));
+        //cout<<cnt<<' '<<ny<<' '<<nx<<' '<<d<<"\n";
+        y=ny;
+        x=nx;
         cnt++;
     }
+    
+    return;
+    
+}
+
+void setting2(){
+
+    path.pop_back();
+    path.push_back(make_tuple(1,1,2));
+    int y=1;
+    int x=1;
+    int cnt=0;
+    int s=n-1;
+    int d=2;
+    
+    for(int j=0;j<n-2;j++){
+        int ny=y+dy[d];
+        int nx=x+dx[d];
+        path.push_back(make_tuple(ny,nx,d));
+        //cout<<cnt<<' '<<ny<<' '<<nx<<' '<<d<<"\n";
+        y=ny;
+        x=nx;
+    }
+    int ny=y+dy[d];
+    int nx=x+dx[d];
+    if(!range(ny,nx))return;
+    d=(d+3)%4;
+    path.push_back(make_tuple(ny,nx,d));
+    //cout<<cnt<<' '<<ny<<' '<<nx<<' '<<d<<"\n";
+    y=ny;
+    x=nx;
+    //cout<<y<<x<<'\n';
+
+    
+    while(1){
+        if(cnt==2){
+            cnt=0;
+            s--;
+        }
+        for(int j=0;j<s-1;j++){
+            int ny=y+dy[d];
+            int nx=x+dx[d];
+            
+            path.push_back(make_tuple(ny,nx,d));
+            if(y==n/2+1&&x==n/2+1)return;
+            //cout<<cnt<<' '<<ny<<' '<<nx<<' '<<d<<"\n";
+            y=ny;
+            x=nx;
+        }
+        int ny=y+dy[d];
+        int nx=x+dx[d];
+        if(y==(n/2+1)&&x==(n/2+1))return;
+        d=(d+3)%4;
+        path.push_back(make_tuple(ny,nx,d));
+        //cout<<cnt<<' '<<ny<<' '<<nx<<' '<<d<<"\n";
+        y=ny;
+        x=nx;
+        cnt++;
+    }
+    
+    
+
+}
+void sett(){
+    path.push_back(make_tuple(3,3,0));
+    setting();
+    setting2();
+    
+    return;
+}
+
+int dist(int cy,int cx,int y,int x){
+    int dis=abs(cy-cx)+abs(y-x);
+    if(dis<=3)return 1;
+    return 0;
+}
+
+void move(int now){
+    map=vector<vector<int>>(n+1,vector<int>(n+1,0));
+
+    int cy,cx,cd;
+    tie(cy,cx,cd)=path[now];
+
+    for(int i=0;i<thief.size();i++){
+        int y,x,d;
+        tie(y,x,d)=thief[i];
+        //cout<<y<<' '<<x<<' '<<d<<'\n';
+        if(!dist(cy,cx,y,x)){
+            map[y][x]++;
+            continue;
+        }
+        int ny=y+dy[d];
+        int nx=x+dx[d];
+        //cout<<ny<<" "<<nx<<'\n';
+        if(!range(ny,nx)){
+            d=(d+6)%4;
+            ny=y+dy[d];
+            nx=x+dx[d];
+        }
+        if(ny==cy&&nx==cx){
+            map[y][x]++;
+            thief[i]=make_tuple(y,x,d);
+            //cout<<y<<" "<<x<<" "<<d<<'\n';
+           
+        }
+        else{
+            map[ny][nx]++;
+            thief[i]=make_tuple(ny,nx,d);
+            //cout<<ny<<" "<<nx<<" "<<d<<'\n';
+            
+        }
+        
+    }
+    return;
+}
+int ans=0;
+void seek(int turn,int now){
+    int cy,cx,cd;
+    tie(cy,cx,cd)=path[now];
+    for(int i=0;i<3;i++){
+        int ny=cy+dy[cd]*i;
+        int nx=cx+dx[cd]*i;
+        if(!range(ny,nx))continue;
+        if(tr[ny][nx]==1)continue;
+        ans+=turn*map[ny][nx];
+        map[ny][nx]=0;
+        
+    }
+    return;
+
+
+}
+void erase(int now){
+    temp=vector<tuple<int,int,int>>();
+    for(int i=0;i<thief.size();i++){
+        int y,x,d;
+        tie(y,x,d)=thief[i];
+        if(map[y][x]==0)continue;
+        temp.push_back(make_tuple(y,x,d));
+
+    }
+    thief=temp;
+    return;
+
+}
+
+void sol(int turn, int now){
+    move(now);
+    //pr();
+    seek(turn,((now+1)%path.size()));
+    //pr();
+    erase((now+1)%path.size());
+    return;
 }
 
 int main() {
-    map=vector<vector<int>>(n+1,vector<int>(n+1));
-    tree=vector<vector<int>>(n+1,vector<int>(n+1));
+    
 
     cin>>n>>m>>h>>k;
+    map=vector<vector<int>>(n+1,vector<int>(n+1));
+    tr=vector<vector<int>>(n+1,vector<int>(n+1));
     
     for(int i=0;i<m;i++){
         int y,x,d;
+        cin>>y>>x>>d;
         thief.push_back(make_tuple(y,x,d));
     }
 
     for(int i=0;i<h;i++){
         int y,x;
-        trees.push_back(make_tuple(y,x));
+        cin>>y>>x;
+        tr[y][x]=1;
     }
 
-    setting();
+    sett();
+
+    for(int i=0;i<k;i++){
+        int now=i%path.size();
+        sol(i+1,now);
+    }
+    /*
     for(int i=0;i<path.size();i++){
         int y,x,d;
         tie(y,x,d)=path[i];
+        cout<<y<<x<<d<<'\n';
     }
+    cout<<path.size()<<'\n';
+    */
+    cout<<ans;
 
 }
