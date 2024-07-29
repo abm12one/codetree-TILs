3a10a7aadc60ae15002e1778dfdd988641d2905a
abm12one <87717167+abm12one@users.noreply.github.com>
2024-07-29 20:52:12 +0900

    Title: hashmap 기본, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240729/hashmap 기본/hashmap-basic.cpp
+++ 240729/hashmap 기본/hashmap-basic.cpp
@@ -1,177 +1,164 @@
-#ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
-#endif
-
-#include <stdio.h>
 #include <iostream>
-#include <cstring>
-#include <algorithm>
+#include <deque>
+#include <memory.h>
 using namespace std;
 
-#define CMD_INIT    1
-#define CMD_APPEND  2
-#define CMD_CUT     3
-#define CMD_REVERSE 4
-#define CMD_COUNT   5
+bool reversed;
+deque<char> str;
+int hashCount[1 << 20];
 
-extern void init(char mStr[]);
-extern void appendWord(char mWord[]);
-extern void cut(int k);
-extern void reverse();
-extern int countOccurrence(char mWord[]);
+void pushBack(char x)
+{
+    str.push_back(x);
 
-char str[150001];
+    int i = str.size() - 1;
+    int cnt = 0;
+    int hash = 0;
+    int exp = 1;
+    while (i > -1 && cnt < 4)
+    {
+        hash += exp * (str[i] - 'a' + 1);
+        exp <<= 5;
+        hashCount[hash]++;
+        i--;
+        cnt++;
+    }
+}
+
+void pushFront(char x)
+{
+    str.push_front(x);
+    int i = 0;
+    int cnt = 0;
+    int hash = 0;
+    int exp = 1;
+    while (str[i] && cnt < 4)
+    {
+        hash <<= 5;
+        hash += (str[i] - 'a' + 1);
+        hashCount[hash]++;
+        i++;
+        cnt++;
+    }
+}
+
+void popBack()
+{
+    int i = str.size() - 1;
+    int cnt = 0;
+    int hash = 0;
+    int exp = 1;
+    while (i > -1 && cnt < 4)
+    {
+        hash += exp * (str[i] - 'a' + 1);
+        exp <<= 5;
+        hashCount[hash]--;
+        i--;
+        cnt++;
+    }
+    str.pop_back();
+}
+
+void popFront()
+{
+    int i = 0;
+    int cnt = 0;
+    int hash = 0;
+    int exp = 1;
+    while (str[i] && cnt < 4)
+    {
+        hash <<= 5;
+        hash += (str[i] - 'a' + 1);
+        hashCount[hash]--;
+        i++;
+        cnt++;
+    }
+    str.pop_front();
+}
 
 void init(char mStr[])
 {
-    strcpy(str,mStr);
-    return;
+    str.clear();
+    reversed = false;
+    memset(hashCount, 0, sizeof(hashCount));
+
+    int i = 0;
+    while (mStr[i])
+    {
+        pushBack(mStr[i]);
+        i++;
+    }
 }
 
 void appendWord(char mWord[])
 {
-    strcat(str,mWord);
-    return;
+    int i = 0;
+    if (reversed)
+    {
+        while (mWord[i])
+        {
+            pushFront(mWord[i]);
+            i++;
+        }
+    }
+    else
+    {
+        while (mWord[i])
+        {
+            pushBack(mWord[i]);
+            i++;
+        }
+    }
 }
 
 void cut(int k)
 {
-    string temp=str;
-    int ts=temp.size();
-    temp=temp.substr(0,ts-k);
-    strcpy(str,temp.c_str());
-    return;
-    
+    int i = 0;
+    if (reversed)
+    {
+        while (i < k)
+        {
+            popFront();
+            i++;
+        }
+    }
+    else
+    {
+        while (i < k)
+        {
+            popBack();
+            i++;
+        }
+    }
 }
 
 void reverse()
 {
-    string temp=str;
-    reverse(temp.begin(),temp.end());
-    strcpy(str,temp.c_str());
-    return;
-
+    reversed = !reversed;
 }
-
 
 int countOccurrence(char mWord[])
 {
-	return 0;
-}
-
-/*
-void pr(){
-    for(int i=0;i<150001;i++){
-        if(str[i]=='\0')break;
-        cout<<str[i];
+    int i = 0;
+    int hash = 0;
+    if (reversed)
+    {
+        int exp = 1;
+        while (mWord[i])
+        {
+            hash += exp * (mWord[i] - 'a' + 1);
+            exp <<= 5;
+            i++;
+        }
     }
-    cout<<"\n";
-    return;
+    else
+    {
+        while (mWord[i])
+        {
+            hash <<= 5;
+            hash += (mWord[i] - 'a' + 1);
+            i++;
+        }
+    }
+    return hashCount[hash];
 }
-*/
-
-int main(){
-    
-    char a[6];
-    char b[6];
-    scanf("%s", a);
-    
-    init(a);
-    pr();
-    
-    scanf("%s", b);
-    appendWord(b);
-    pr();
-
-    cut(3);
-    pr();
-    reverse( );
-    pr();
-
-
-}
-
-
-
-/*
-static bool run()
-{
-	bool correct = false;
-	int queryCnt;
-	scanf("%d", &queryCnt);
-	static char mStr[30001], mWord[5];
-
-	while (queryCnt--)
-	{
-		int cmd;
-		scanf("%d", &cmd);
-
-		if (cmd == CMD_INIT)
-		{
-			scanf("%s", mStr);
-			init(mStr);
-			correct = true;
-		}
-		else if (cmd == CMD_APPEND)
-		{
-			scanf("%s", mWord);
-			
-			if (correct)
-			{
-				appendWord(mWord);
-			}
-		}
-		else if (cmd == CMD_CUT)
-		{
-			int k;
-			scanf("%d", &k);
-			
-			if (correct)
-			{
-				cut(k);
-			}
-		}
-		else if (cmd == CMD_REVERSE)
-		{
-			if (correct)
-			{
-				reverse();
-			}
-		}
-		else if (cmd == CMD_COUNT)
-		{
-			scanf("%s", mWord);
-
-			int ret = -1;
-			if (correct)
-			{
-				ret = countOccurrence(mWord);
-			}
-
-			int ans;
-			scanf("%d", &ans);
-			if(ret != ans)
-			{
-				correct = false;
-			}
-		}
-	}
-	return correct;
-}
-
-int main()
-{
-	setbuf(stdout, NULL);
-	//freopen("sample_input.txt", "r", stdin);
-
-	int T, MARK;
-	scanf("%d %d", &T, &MARK);
-
-	for (int tc = 1; tc <= T; tc++)
-	{
-		int score = run() ? MARK : 0;
-		printf("#%d %d\n", tc, score);
-	}
-	return 0;
-}
-*/
